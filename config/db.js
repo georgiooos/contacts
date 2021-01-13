20b3e932c6bacd4e3a8f2cfcab76ddeff873e066
george <geoliat@yahoo.com>
2021-01-14 00:02:44 +0200

    update

--- config/db.js
+++ config/db.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
  
-const connectDB = () =>{
-    mongoose.connect(db,{
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }).then(()=>console.log('Mongo DB Connected... GEORGE !!!'))
-    .catch(err=>{
+const connectDB = async() =>{
+    try {
+        await mongoose
+        .connect(db,{
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
+        console.log('Mongo DB Connected... GEORGE !!!');        
+    } catch (err) {
         console.error(err.message);
-        process.exit(1);
-    })
+        process.exit(1);        
+    }
 
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express=require('express');
 const router=express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//route: GET api/users
+const User = require('../models/User');
+
+//route: POST api/users
 //desc register user
 //access public
-router.get('/',(req,res)=>{
-    res.send('register user');
-});
+router.post(
+    '/',
+    [
+        check('name','mame?')
+            .not()
+            .isEmpty(),
+        check('email','email?')
+            .isEmail(),    
+        check('password','pass?')
+            .isLength({min:6})  
+    ],
+    (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        res.send('passed');
+    }
+);
 
 module.exports = router;
