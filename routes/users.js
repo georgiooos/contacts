d4d889a1bdad4f7acd42e739a5cfbf770edf5568
george <geoliat@yahoo.com>
2021-01-14 00:45:06 +0200

    os ke 43, save user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -18,12 +19,38 @@ router.post(
         check('password','pass?')
             .isLength({min:6})  
     ],
-    (req,res)=>{
+    async (req,res)=>{
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()})
         }
-        res.send('passed');
+
+        const {name,email,password}=req.body;
+
+        try {
+            let user = await User.findOne({email})
+
+            if(user){
+                return res.status(400).json({msg:'exists'})
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            })
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password,salt);
+
+            await user.save();
+
+            res.send('saved user')
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('error from send user and encrypt try')
+        }
+        //res.send('passed');
     }
 );
 
