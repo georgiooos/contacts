f19054e5ddf4d1866f79cccbc708847db5eae4b1
george <geoliat@yahoo.com>
2021-01-18 01:05:26 +0200

    os ke 48, add contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,9 +23,44 @@ router.get('/',auth,async(req,res)=>{
 //route: POST api/contacts
 //desc add contact
 //access private
-router.post('/',(req,res) =>{
-    res.send('add contact');
-});
+router.post(
+    '/',
+    [
+        auth,
+        [
+            check('name','name required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req,res) =>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        
+        const {name,email,phone,type}=req.body;
+
+        try {
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user: req.user.id
+            });
+
+            const contact = await newContact.save();
+
+            res.json(contact);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('server error');
+        }
+        
+    //res.send('add contact');
+    }
+);
 
 //route: PUT api/contacts
 //desc update contact
