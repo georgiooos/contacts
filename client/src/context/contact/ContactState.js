f27771e06a1ab6f22701b10aa96c525ab6f50aef
george <geoliat@yahoo.com>
2021-02-28 19:38:53 +0200

    70 > delete-addfirst

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -57,14 +57,21 @@ const ContactState = props => {
         } catch (err) {
             dispatch({type:CONTACT_ERROR,payload:err.response.msg})
         }
-
-
     }
 
     //delete contact
 
-    const deleteContact = id =>{
-        dispatch({type: DELETE_CONTACT, payload:id});
+    const deleteContact = async id =>{
+
+        try {
+            await axios.delete(`/api/contacts/${id}`)
+
+            dispatch({type: DELETE_CONTACT, payload:id});
+        } catch (err) {
+            dispatch({type:CONTACT_ERROR,payload:err.response.msg})
+        }
+
+
     }
 
     //clear contacts
